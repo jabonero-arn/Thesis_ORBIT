8be1b2b046541086b05f0a0884d4b74870eb719d
Sikwate <christianjayefernan@gmail.com>
2026-03-26 16:06:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "labmat22-90952233-b068b",
+  "appId": "1:850994451949:web:c6252ca43be250d05b83e4",
+  "apiKey": "AIzaSyCtDY2ysu1rt75ei5SqULvjDM9MhF6nogw",
+  "authDomain": "labmat22-90952233-b068b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "850994451949"
+};
